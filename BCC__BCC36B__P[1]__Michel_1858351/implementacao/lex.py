fa79d6b8215f2e03d7409a4c86c289ce84bc250b
Michel G. Souza <Michelgomesdes@hotmail.com>
2021-03-09 17:58:14 -0300

    Comentando os Comentarios e outras solucoes

--- BCC__BCC36B__P[1]__Michel_1858351/implementacao/lex.py
+++ BCC__BCC36B__P[1]__Michel_1858351/implementacao/lex.py
@@ -1,7 +1,11 @@
+# -*- coding: utf-8 -*-
+chave = 0
 import ply.lex as lex
 import sys
+import re
 
 tokens = (
+'NUM_INTEIRO',
 'MAIS',
 'MENOS',
 'MULTIPLICACAO',
@@ -12,7 +16,6 @@ tokens = (
 'MAIOR',
 'IGUAL',
 'DIFERENTE',
-'FIM',
 'MENOR_IGUAL',
 'MAIOR_IGUAL',
 'E_LOGICO',
@@ -33,49 +36,98 @@ tokens = (
 'RETORNA',
 'INTEIRO',
 'FLUTUANTE',
-'NUM_INTEIRO',
 'NUM_PONTO_FLUTUANTE',
 'NUM_NOTACAO_CIENTIFICA',
 'ID',
 'ABRE_CHAVES',
+'FIM',
 'FECHA_CHAVES',
 'COMENTARIO'
 )
 
+t_SE = r'(se)[^\w+]'
+t_SENAO = r'(senão)'
+t_ATE = r'até'
+t_LEIA = r'leia'
+t_ENTAO = r'então'
+t_REPITA = r'repita'
+t_ESCREVA = r'escreva'
+t_RETORNA = r'retorna'
+t_INTEIRO = r'inteiro'
+t_FLUTUANTE = r'flutuante'
+t_MENOR = r'<'
+t_MAIOR = r'>'
+t_IGUAL = r'='
+t_DIVISAO = r'/'
+t_VIRGULA = r','
+t_NEGACAO = r'!'
+t_E_LOGICO = r'&&'
+t_DIFERENTE = r'<>'
+t_OU_LOGICO = r'\|\|'
+t_ATRIBUICAO = r':='
+t_DOIS_PONTOS = r':'
+t_MENOR_IGUAL = r'<='
+t_MAIOR_IGUAL = r'>='
+#t_COMENTARIO = r'\{[\w\n\r\s\t]+.*\}'
+t_MULTIPLICACAO = r'\*'
+t_ABRE_COLCHETE = r'\['
+t_FECHA_COLCHETE = r'\]'
+t_ABRE_PARENTESE = r'\('
+t_FECHA_PARENTESE = r'\)'
+t_ID = r'\w+'
 
-def t_NUM_INTEIRO(t):
-    #r'\d+$'
-    #r'^(\d)|(-\d)'
+t_ignore  = " \t"
 
-    #r'^(\d)|((\s|\t|\n)?-\d)'
 
-   
-    #r'[^(\d+)(\+|\-)(\d+)]([^(:=\s)][-]\d+)|(\d+)'
-    
-    
-    #r'([^(\d+)\-(\d+)]([-]\d+))'
-    r'([^(\d+)]?([-]?\d+))[^(\d+)\-(\d+)]'
-    
-    
-    #r'[^(\d+)(\+|\-)(\d+)](-\d+)|(\d+)'
-    #r'(^(\+|-)?(\d+))' reconhece só o d+
-    #r'(^(-|\+)?(\d+$))'
-
-    #r'((\s|\t|\n)-\d)'
-    #t.value = t.value.replace(":=","")
-
-    t.value = int(t.value)    
+def t_NUM_NOTACAO_CIENTIFICA(t):
+    r'\d+\^+\d+'
+    t.value = t.value    
     return t
 
+def t_NUM_PONTO_FLUTUANTE(t):
+    r'\d+\.\d*' 
+    t.value = float(t.value)    
+    return t
+    
+def t_COMENTARIO(token):
+    r"(\{((.|\n)*?)\})"
+    token.lexer.lineno += token.value.count("\n")
+    # return token
+
+def t_NUM_INTEIRO(t):
+    r'((?<=\D)[+-]\d+)|(?<=\W)\d+'
+    t.value = int(t.value)    
+    return t
+#https://regex101.com/r/Ebg5LT/2
+
+def t_FIM(token):
+	r'fim'
+	return token
+
+def t_MAIS(t):
+    #r'\+'
+    r'\+'
+    return t
+
+def t_MENOS(t):
+    r'-'
+    return t
+
+#def t_error(t):
+#    print("Caracter inválido '%s'" % t.value[0])
+#    t.lexer.skip(1)
+
+
 def t_newline(t):
-    r'\n+'
-    t.lexer.lineno += len(t.value)
+	r"\n+"
+	t.lexer.lineno += len(t.value)
 
-t_ignore  = ' \t'
+def t_error(token):
 
-def t_error(t):
-    print("Caractere Ilegal '%s'" % t.value[0])
-    t.lexer.skip(1)
+    line = token.lineno
+    message ="Caracter inválido '%s'" % token.value[0]
+    print(message)
+    token.lexer.skip(1)
 
 lexer = lex.lex()
 
@@ -86,9 +138,11 @@ def proxToken(data):
     while True:
         tok = lexer.token()
         if not tok: 
-            break      
-        print(tok.type, tok.value)
-        #print(tok.type)
+            break
+        if(chave == 1):
+            print(tok.type, tok.value)#print(tok.type, tok.value)
+        else:
+            print(tok.type)
 
 def main():
     
